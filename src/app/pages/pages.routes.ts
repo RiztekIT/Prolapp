05b582196f24cb2333cb986a45408838ebd30fb6
Javier Gutierrez <javier.gutierrez@riztek.com.mx>
2019-11-19 18:01:58 -0700

    Trafico calendario y pedido

--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -19,21 +19,18 @@ import { EvidenciasComponent } from './calidad/evidencias/evidencias.component';
 import { DashboardVentasComponent } from './ventas/dashboard-ventas/dashboard-ventas.component';
 import { CalendarioVentasComponent } from './ventas/calendario-ventas/calendario-ventas.component';
 import { ProfileComponent } from '../components/profile/profile.component';
-<<<<<<< HEAD
 import { PedidoTraficoComponent } from './trafico/pedido-trafico/pedido-trafico.component';
 import { ReporteTraficoComponent } from './trafico/reporte-trafico/reporte-trafico.component';
 import { FormatofacturaTraficoComponent } from './trafico/formatofactura-trafico/formatofactura-trafico.component';
 import { DashboardTraficoComponent } from './trafico/dashboard-trafico/dashboard-trafico.component';
 import { CalendarioTraficoComponent } from './trafico/calendario-trafico/calendario-trafico.component';
-=======
 import { CatalogosComponent } from './administracion/catalogos/catalogos.component';
 import { PermisosComponent } from './administracion/permisos/permisos.component';
->>>>>>> e97cdec731106d602a7ba80e0dc344a5c1c8e7db
 
 
 
 const pagesRoutes: Routes = [
-    { 
+    {
             path: '', component: PagesComponent,
             children: [
                 { path: 'dashboard', component: DashboardComponent },
