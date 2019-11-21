93c0d843d90348cafd68ec11159c01b397214181
javier sierra <javier.sierra@riztek.com.mx>
2019-11-21 11:04:22 -0700

    Embarque y documentacion actualizados

--- src/app/pages/importacion/embarque-importacion/embarque-importacion.component.ts
+++ src/app/pages/importacion/embarque-importacion/embarque-importacion.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+
+declare function steps();
+declare function upload();
 
 @Component({
   selector: 'app-embarque-importacion',
@@ -7,9 +12,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmbarqueImportacionComponent implements OnInit {
 
-  constructor() { }
+  ciudades: any = [
+    'Seleccionar Ciudad',
+    'Aguascalientes',
+    'Baja California',
+    'Baja California Sur',
+    'Campeche',
+    'Coahuila',
+    'Colima',
+    'Chiapas',
+    'Chihuahua',
+    'Distrito Federal',
+    'Durango',
+    'Guanajuato',
+    'Guerrero',
+    'Hidalgo',
+    'Jalisco',
+    'México',
+    'Michoacán de Ocampo',
+    'Morelos',
+    'Nayarit',
+    'Nuevo León',
+    'Oaxaca',
+    'Puebla',
+    'Querétaro',
+    'Quintana Roo',
+    'San Luis Potosí',
+    'Sinaloa',
+    'Sonora',
+    'Tabasco',
+    'Tamaulipas',
+    'Tlaxcala',
+    'Veracruz',
+    'Yucatán',
+    'Zacatecas'
+];
+
+  constructor(public router: Router) { }
 
   ngOnInit() {
+    steps();
+    upload();
   }
 
 }
