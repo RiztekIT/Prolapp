f7d6c8edff96faf7648b5ab031e8b30b4caa6191
Javier Gutierrez <javier.gutierrez@riztek.com.mx>
2019-11-26 11:56:58 -0700

    Compras modificado

--- src/app/pages/compras/formato-compras-compras/formato-compras-compras.component.ts
+++ src/app/pages/compras/formato-compras-compras/formato-compras-compras.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+declare function steps();
+declare function datepicker();
 
 @Component({
   selector: 'app-formato-compras-compras',
@@ -7,9 +11,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormatoComprasComprasComponent implements OnInit {
 
-  constructor() { }
+  ciudades: any = [
+    'Seleccionar Ciudad',
+    'Aguascalientes',
+    'Baja California',
+    'Baja California Sur',
+    'Campeche',
+    'Coahuila',
+    'Colima',
+    'Chiapas',
+    'Chihuahua',
+    'Distrito Federal',
+    'Durango',
+    'Guanajuato',
+    'Guerrero',
+    'Hidalgo',
+    'Jalisco',
+    'México',
+    'Michoacán de Ocampo',
+    'Morelos',
+    'Nayarit',
+    'Nuevo León',
+    'Oaxaca',
+    'Puebla',
+    'Querétaro',
+    'Quintana Roo',
+    'San Luis Potosí',
+    'Sinaloa',
+    'Sonora',
+    'Tabasco',
+    'Tamaulipas',
+    'Tlaxcala',
+    'Veracruz',
+    'Yucatán',
+    'Zacatecas'
+];
+  constructor(public router: Router) { }
 
   ngOnInit() {
+    steps();
+    datepicker();
+  }
+  recargar() {
+    // this.router.navigate(['/pedidoVentas']);
+    this.ngOnInit();
   }
 
 }
