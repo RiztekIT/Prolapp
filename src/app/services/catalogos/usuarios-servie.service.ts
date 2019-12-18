1bd3134992ef94ad8fa69e521d165851a92db0f0
javier sierra <javier.sierra@riztek.com.mx>
2019-12-18 11:02:17 -0700

    modificacion de puertos

--- src/app/services/catalogos/usuarios-servie.service.ts
+++ src/app/services/catalogos/usuarios-servie.service.ts
@@ -13,8 +13,8 @@ export class UsuariosServieService {
   constructor(private http:HttpClient) { }
   formData: Usuario;
 
-  // readonly APIUrl = "https://localhost:44361/api";
-  readonly APIUrl = "http://192.168.1.67:7002/api";
+  readonly APIUrl = "https://localhost:44361/api";
+  // readonly APIUrl = "http://192.168.1.67:7002/api";
 
 
   getUsuariosList(): Observable <Usuario[]> {
