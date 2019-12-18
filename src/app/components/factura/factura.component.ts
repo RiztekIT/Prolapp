7fb1e87b370aa910cba40445f9a2e1312427cac7
Javier Gutierrez <javier.gutierrez@riztek.com.mx>
2019-12-18 10:21:49 -0700

    xml

--- src/app/components/factura/factura.component.ts
+++ src/app/components/factura/factura.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import xml2js  from 'xml2js';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { DomSanitizer } from '@angular/platform-browser';
 
 
 @Component({
@@ -7,11 +10,65 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./factura.component.css']
 })
 export class FacturaComponent implements OnInit {
-  constructor() {
+  constructor(private _http: HttpClient, private sanitizer: DomSanitizer) {
+    this.loadXML();
+  }
+  title = 'xmljson';
+  public xmlItems: any;
+  fileUrl:any;
+  QRString = 'CodigoQREjemplo';
+  loadXML(){
+    this._http.get('/assets/F-1.xml',
+    {
+      headers: new HttpHeaders()
+      .set('Content-Type', 'text/xml')
+      .append('Access-Control-Allow-Methods', 'GET')
+      .append('Access-Control-Allow-Origin', '*')
+      .append('Access-Control-Allow-Headers', "Access-Control-Allow-Headers, Access-Control-Allow-Origin, Access-Control-Request-Method"),
+    responseType: 'text'
+    })
+    .subscribe((data) =>{
+      this.parseXML(data)
+      .then((data) => {
+        this.xmlItems = data;
+      });
+    });
   }
 
-  ngOnInit() {
+  parseXML(data){
+    return new Promise(resolve => {
+      let k: string | number,
+      arr = [],
+      parser = new xml2js.Parser(
+        {
+          trim:true,
+          explicitArray: true
+        }); 
+        parser.parseString(data, function (err, result){
+          var obj = result.Employee;
+          for (k in obj.emp) {
+            var item = obj.emp[k];
+            arr.push({
+            //   id: item.id[0],  
+            // name: item.name[0],  
+            // gender: item.gender[0],  
+            // mobile: item.mobile[0]
+            
+            });
+          }
+          resolve(arr);
+        })
+    })
   }
-  QRString = 'CodigoQREjemplo';
+
+
+
+
+  ngOnInit() {
+    const blob = new Blob(['/assets/F-1.xml'], { type: 'application/octet-stream' });
+  
+    this.fileUrl = this.sanitizer.bypassSecurityTrustResourceUrl(window.URL.createObjectURL(blob));
+  }
+QRstring = ""; 
 
 }
