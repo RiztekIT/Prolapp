f26de1001c12264bf0f52c91279451d1f5ddd1fd
Ivan Talamantes <ivan.talamantes@riztek.com.mx>
2019-12-18 10:10:18 -0700

    proxy y cambios factura

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Form, NgForm } from '@angular/forms';
+import { Usuario } from '../Models/catalogos/usuarios-model';
+import { UsuariosServieService } from '../services/catalogos/usuarios-servie.service';
+
+
 
 declare function init_plugins();
 
@@ -10,19 +15,49 @@ declare function init_plugins();
 })
 export class LoginComponent implements OnInit {
 
-  constructor(public router: Router) { }
+  constructor(public router: Router, public service: UsuariosServieService) { }
 
   ngOnInit() {
 
     init_plugins();
+    this.resetForm();
 
   }
 
-  ingresar(){
-    // this.router.navigateByUrl('progress');    
-    // this.router.navigate(['/dashboard']);
-    this.router.navigate(['/direccion']);
-    // this.http.post('#');
+  // ingresar(){
+  //   // this.router.navigateByUrl('progress');    
+  //   // this.router.navigate(['/dashboard']);
+  //   this.router.navigate(['/direccion']);
+  //   // this.http.post('#');
+
+  // }
+
+  autentificar(form: NgForm){
+    console.log(form.value);
+
+    this.service.getLogin(form.value).subscribe(data => {
+     this.resetForm(form);
+     console.log(data);
+    //console.log(this.listData);
+    });
+    
+
+  }
+
+  resetForm(form?: NgForm) {
+    if (form != null)
+   form.resetForm();
+   this.service.formData = {
+    IdUsuario: 0,
+    Nombre: '',
+    NombreUsuario: 'IvanTa',
+    ApellidoPaterno: '',
+    ApellidoMaterno: '',
+    Correo: '',
+    Telefono: '',
+    Contra: 'Ivan2019',
+    FechaUltimoAcceso: '2019-12-19'
+   }
 
   }
 
---
+++ src/app/services/facturacioncxc/enviarfactura.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Factura } from '../../Models/facturacioncxc/factura-model';
+import { Observable } from 'rxjs';
+
+
+  const httpOptions = {
+    headers: new HttpHeaders({
+      'F-Api-Key':'JDJ5JDEwJGZOWTRnNkdvSjBPTEdiRlRBNWZocE81d3dJRU52WUtNWU9SaU16MHcwbFV5MzIuVWVGTlBT',
+      'F-Secret-Key':'JDJ5JDEwJGhVemxJbXUyTzhUREVTTEVvODkySk91aEI4a3Y0Rjhqd3ltWHo0a0QyTktTdkhldEp2c29X',
+      'Access-Control-Allow-Origin': 'http://192.168.1.67:4200',
+      'Content-Type': 'application/json;charset=UTF-8',
+      'Access-Control-Allow-Headers': 'F-Secret-Key,Accept, Accept-Encoding, Content-Type',
+      'Access-Control-Allow-Methods': 'GET, OPTIONS'
+    })
+  }
+    
+    
+
+
+  
+  
+
+
+@Injectable({
+  providedIn: 'root'
+})
+
+
+export class EnviarfacturaService {
+  // parche: string = 'https://cors-anywhere.herokuapp.com/'
+  // parche: string = ''
+  // readonly rootURL = this.parche +  "http://devfactura.in/api/v3/cfdi33/create"
+  readonly rootURL = "/api/v3/cfdi33/create"
+
+  constructor(private http: HttpClient) { }
+
+  enviarFactura(datos:string): Observable<any>{  
+    console.log(this.http.post(this.rootURL,datos,httpOptions));
+    return this.http.post(this.rootURL,datos,httpOptions);  
+  }
+
+  xml(url:string){
+    return this.http.get(url)
+  }
+
+  
+
+
+}
