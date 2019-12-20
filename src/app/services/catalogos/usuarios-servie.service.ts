65189fe9190c899de970de70030502048ef82b6e
Ricardo <ricardo.delarosa@riztek.com.mx>
2019-12-19 18:03:33 -0700

    showUsuario(PlaceHolders) y API SERVICIOS

--- src/app/services/catalogos/usuarios-servie.service.ts
+++ src/app/services/catalogos/usuarios-servie.service.ts
@@ -14,8 +14,8 @@ export class UsuariosServieService {
   formData: Usuario;
 
   // readonly APIUrl = "https://localhost:44361/api";
-  readonly APIUrl = "http://192.168.1.67:32767/api";
-  // readonly APIUrl = "https://localhost:44361/api";
+  // readonly APIUrl = "http://192.168.1.67:32767/api";
+   readonly APIUrl = "https://localhost:44361/api";
   // readonly APIUrl = "http://192.168.1.67:7002/api";
 
 
