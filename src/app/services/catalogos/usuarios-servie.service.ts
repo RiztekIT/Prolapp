b7f81bfeb5513b9737c73eb5f69c9e33072fc936
RiztekTI <riztekti@gmail.com>
2019-12-20 18:24:09 -0700

    cambios

--- src/app/services/catalogos/usuarios-servie.service.ts
+++ src/app/services/catalogos/usuarios-servie.service.ts
@@ -14,7 +14,8 @@ export class UsuariosServieService {
   formData: Usuario;
 
   // readonly APIUrl = "https://localhost:44361/api";
-  readonly APIUrl = "http://192.168.1.180:44361/api";
+  readonly APIUrl = "http://riztekserver.ddns.net:44361/api";
+  //readonly APIUrl = "http://192.168.1.180:44361/api";
   // readonly APIUrl = "https://localhost:44361/api";
   // readonly APIUrl = "https://localhost:44361/api";
   // readonly APIUrl = "http://192.168.1.67:32767/api";
