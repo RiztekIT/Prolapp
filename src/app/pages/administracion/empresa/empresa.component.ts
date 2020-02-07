99fbef9cbad62c274bc2cd447c252a79ef66ff9c
RiztekIT <javier.gutierrez@riztek.com.mx>
2020-02-07 16:10:14 -0700

    Empresas

--- src/app/pages/administracion/empresa/empresa.component.ts
+++ src/app/pages/administracion/empresa/empresa.component.ts
@@ -1,4 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild} from '@angular/core';
+import { Router } from '@angular/router';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {MatTableDataSource, MatPaginator, MatTable, MatDialog, MatSnackBar, MatDialogConfig} from '@angular/material';
+import {MatSort} from '@angular/material/sort';
+import { trigger, state, transition, animate, style } from '@angular/animations';
+import Swal from 'sweetalert2';
+import { Empresa } from 'src/app/Models/Empresas/empresa-model';
+import { EmpresaService } from 'src/app/services/empresas/empresa.service';
+import { DomSanitizer } from '@angular/platform-browser';
+
+import { AddEmpresaComponent } from './add-empresa/add-empresa.component';
+import { ShowEmpresaComponent } from './show-empresa/show-empresa.component';
+
 
 @Component({
   selector: 'app-empresa',
@@ -7,9 +20,103 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmpresaComponent implements OnInit {
 
-  constructor() { }
+  empresaFoto: any;
 
-  ngOnInit() {
+  constructor(public router: Router, private dialog: MatDialog, public service: EmpresaService, private _formBuilder: FormBuilder, private sanitizer: DomSanitizer ) { 
+
+    this.service.listen().subscribe((m:any)=>{
+      this.refreshEmpresaList();
+    });
+
   }
 
+  ngOnInit() {
+    this.refreshEmpresaList();
+    this.refreshEmpresaFoto();
+  }
+
+  listData: MatTableDataSource<any>;
+  displayedColumns: string [] = ['Imagen', 'RazonSocial' ,'RFC', 'Calle', 'NumeroInterior', 'NumeroExterior', 'CP', 'Colonia', 'Ciudad', 'Estado', 'Pais', 'Regimen', 'Options'];
+
+  @ViewChild(MatSort, null) sort : MatSort;
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+
+
+  refreshEmpresaList(){
+
+    this.service.getEmpresaList().subscribe(data =>{
+      //console.log(data[0]);
+      this.service.formData = data[0];
+      this.listData = new MatTableDataSource(data);
+      this.listData.sort = this.sort;
+      this.listData.paginator = this.paginator;
+      this.listData.paginator._intl.itemsPerPageLabel = 'Empresas por Pagina';
+    })
+  }
+
+  refreshEmpresaFoto(foto?: string){
+    console.log(this.service.getEmpresaFoto());
+    this.service.getEmpresaFoto().subscribe(data => {
+      console.log(data);
+      
+     let objectURL = data[0].Foto;
+     // let objectURL = foto;
+
+         this.empresaFoto = this.sanitizer.bypassSecurityTrustUrl(objectURL);
+
+
+    })
+  }
+
+
+  onDelete( id:number ){
+
+    Swal.fire({
+      title: '¿Seguro que quiere eliminar la empresa?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Borrar',
+      cancelButtonText: 'Cancelar'
+    }).then((result)=>{
+      if (result.value) {
+        // this.service.deleteEmpresa(id).subscribe(res => {
+        //   this.refreshEmpresaList();
+        //   Swal.fire({
+        //     title: ' Empresa Eliminada',
+        //     icon: 'success',
+        //     timer: 1000,
+        //     showCancelButton: false,
+        //     showConfirmButton: false
+        // });
+        //   });
+      }
+    })
+  }
+
+  onAdd(){
+
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+    dialogConfig.width="70%";
+    this.dialog.open(AddEmpresaComponent, dialogConfig);
+
+  }
+
+  onEdit(empresa: Empresa){
+    // console.log(usuario);
+    this.service.formData = empresa;
+        const dialogConfig = new MatDialogConfig();
+        dialogConfig.disableClose = true;
+        dialogConfig.autoFocus = true;
+        dialogConfig.width="70%";
+        this.dialog.open(ShowEmpresaComponent, dialogConfig);
+      }
+    
+      applyFilter(filtervalue: string){  
+        this.listData.filter= filtervalue.trim().toLocaleLowerCase();
+    
+      }
 }
