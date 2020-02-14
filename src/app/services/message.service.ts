8727b61e31c43a61a2284b816de6d97a9f5fa12e
Ivan Talamantes <ivan.talamantes@riztek.com.mx>
2020-02-13 19:02:10 -0700

    enviar correo

--- src/app/services/message.service.ts
+++ src/app/services/message.service.ts
@@ -7,12 +7,23 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class MessageService {
+  
+  
 
   constructor(private _http: HttpClient) { }
-
+  nombre;
+  correo;
+  cco;
+  asunto;
+  cuerpo;
   sendMessage(body){
     console.log(body);
     
     return this._http.post("http://riztekserver.ddns.net:3000/formulario", body)
   }
+  // sendMessage2(body,files){
+  //   console.log(body);
+    
+  //   return this._http.post("http://riztekserver.ddns.net:3000/formulario", body,files)
+  // }
 }
