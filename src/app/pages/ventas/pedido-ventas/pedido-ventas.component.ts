22e7a58c920dbe485243bcd52df6f3e4dce4af4c
Ivan Talamantes <ivan.talamantes@riztek.com.mx>
2020-02-17 10:50:41 -0700

    arreglar para safari

--- src/app/pages/ventas/pedido-ventas/pedido-ventas.component.ts
+++ src/app/pages/ventas/pedido-ventas/pedido-ventas.component.ts
@@ -23,7 +23,8 @@ import { DetallePedido } from '../../../Models/Pedidos/detallePedido-model';
   styleUrls: ['./pedido-ventas.component.css'],
   animations: [
     trigger('detailExpand', [
-      state('collapsed', style({ height: '0px', minHeight: '0', display: 'none' })),
+      // state('collapsed', style({ height: '0px', minHeight: '0', display: 'none' })),
+      state('collapsed', style({ height: '0px', minHeight: '0', visibility: 'hidden' })),
       state('expanded', style({ height: '*' })),
       transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
     ]),
