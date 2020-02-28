81aa3349c7828d4bd5f06212b7a04db282438b6f
RiztekIT <javier.gutierrez@riztek.com.mx>
2020-02-28 16:10:48 -0700

    NotaCredito ♥♥

--- src/app/Models/nota-credito/notaCreditoMaster-model.ts
+++ src/app/Models/nota-credito/notaCreditoMaster-model.ts
@@ -2,8 +2,46 @@ import { NotaCredito } from './notaCredito-model';
 import { DetalleNotaCredito } from './detalleNotaCredito-model';
 
 export class NotaCreditoMaster {
-
-    NotaCredito?: NotaCredito[];
-    DetalleNotaCredito?: DetalleNotaCredito[];
+    IdNotaCredito: number;
+    IdCliente: number;
+    IdFactura: number;
+    Serie: string;
+    Folio: string;
+    Tipo: string;
+    FechaDeExpedicion: Date;
+    LugarDeExpedicion: string;
+    Certificado: string;
+    NumeroDeCertificado: string;
+    UUID: string;
+    UsoDelCFDI: string;
+    Subtotal: string;
+    Descuento: string;
+    ImpuestosRetenidos: string;
+    ImpuestosTrasladados: string;
+    Total: string;
+    FormaDePago: string;
+    MetodoDePago: string;
+    Cuenta: string;
+    Moneda: string;
+    CadenaOriginal: string;
+    SelloDigitalSAT: string;
+    SelloDigitalCFDI: string;
+    NumeroDeSelloSAT: string;
+    RFCDelPAC: string;
+    Observaciones: string;
+    FechaVencimiento: Date;
+    OrdenDeCompra: string;
+    TipoDeCambio: string;
+    FechaDeEntrega: Date;
+    CondicionesDePago: string;
+    Vendedor: string;
+    Estatus: string;
+    Ver: string;
+    Usuario: string;
+    SubtotalDlls: string;
+    ImpuestosTrasladadosDlls: string;
+    TotalDlls: string;  
+    Relacion: string;
+    DetalleNotaCredito?: DetalleNotaCredito[]
     
 }
--- src/app/services/cuentasxcobrar/NotasCreditocxc/notaCredito.service.ts
+++ src/app/services/cuentasxcobrar/NotasCreditocxc/notaCredito.service.ts
@@ -15,6 +15,11 @@ export const APIUrl = "http://riztekserver.ddns.net:44361/api";
 
     constructor(private http:HttpClient) { }
 
+
+    //Master
+
+    master = new Array<NotaCreditoMaster>();
+
     //form Data Nota Credito
     formData = new NotaCredito();
 
@@ -31,9 +36,26 @@ export const APIUrl = "http://riztekserver.ddns.net:44361/api";
 
     readonly APIUrl = "http://riztekserver.ddns.net:44361/api";
 
+    //Get Join Notas y Detalle Notas
+
+  getNotasjoinDetalle(): Observable<any[]>{
+   return this.http.get<[]>(this.APIUrl + '/NotaCredito');
+  }
+
+  getNotaCreditoDetalles(id: number): Observable <DetalleNotaCredito[]> {
+    return this.http.get<DetalleNotaCredito[]>(this.APIUrl + '/NotaCredito/GetDetalleNotaCredito/'+ id);
+  }
+
      //Insertar nueva Nota Credito
   addNotaCredito(notaCredito: NotaCredito) {
     return this.http.post(this.APIUrl + '/NotaCredito', notaCredito);
+  }
+
+  deleteNotaCredito(id: number){
+    return this.http.delete(this.APIUrl + '/NotaCredito/' + id);
+  }  
+  deleteNotaCreada() {
+    return this.http.delete(this.APIUrl + '/NotaCredito/DeleteNotaCreada/');
   }
 
   private _listeners = new Subject<any>(); 
