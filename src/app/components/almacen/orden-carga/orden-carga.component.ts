6f0f741cd4a5ee622dee9cb03a1cabdd97df7045
Rcdelarosa <ricardo.delarosa@riztek.com.mx>
2020-03-23 15:17:55 -0700

    OrdenCargaPDF

--- src/app/components/almacen/orden-carga/orden-carga.component.ts
+++ src/app/components/almacen/orden-carga/orden-carga.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import * as html2pdf from 'html2pdf.js';
+import { MatDialogRef } from '@angular/material';
+import { OrdenCargaService } from 'src/app/services/almacen/orden-carga/orden-carga.service';
+// import { OrdenCarga } from '../../../Models/almacen/OrdenCarga/ordencarga.model';
+// import { DetalleOrdenCarga } from '../../../Models/almacen/OrdenCarga/detalleOrdenCarga-model';
 
 @Component({
   selector: 'app-orden-carga',
@@ -7,21 +11,64 @@ import * as html2pdf from 'html2pdf.js';
   styleUrls: ['./orden-carga.component.css']
 })
 export class OrdenCargaComponent implements OnInit {
+  usda: any;
+  pesoTotal: any;
 
-  constructor() { }
+  constructor(public dialogbox: MatDialogRef<OrdenCargaComponent>, public service: OrdenCargaService) { }
+
+  objconc: any; 
+  con : string| number;
+  arrcon: Array<any> = [];
 
   ngOnInit() {
+    this.ver();
   }
+
+  onClose() {
+    this.dialogbox.close();
+}
+
+ver(){
+
+  console.log(this.service.formrow.detalleOrdenCarga);
+  
+  this.objconc = this.service.formrow.detalleOrdenCarga;
+  
+  this.arrcon = [];
+  for (this.con in this.objconc){
+    var conceptos = this.objconc[this.con];
+    this.arrcon.push({
+      IdDetalleOrdenCarga: conceptos.IdDetalleOrdenCarga,
+      IdOrdenCarga: conceptos.IdOrdenCarga,
+      ClaveProducto: conceptos.ClaveProducto,
+      Producto: conceptos.Producto,
+      Sacos: conceptos.Sacos,
+      PesoxSaco: conceptos.PesoxSaco,
+      Lote: conceptos.Lote,
+      IdProveedor: conceptos.IdProveedor,
+      Proveedor: conceptos.Proveedor,
+      PO: conceptos.PO,
+      FechaMFG: conceptos.FechaMFG,
+      FechaCaducidad: conceptos.FechaCaducidad,
+      Shipper: conceptos.Shipper,      
+      USDA: conceptos.USDA,      
+      Pedimento: conceptos.Pedimento,      
+      Saldo: conceptos.Saldo,      
+    });
+  }
+  this.usda = conceptos.USDA;
+  this.pesoTotal = +conceptos.Sacos * +conceptos.PesoxSaco;
+}
 
   
 onExportClick(Folio?:string) {
   const content: Element = document.getElementById('OrdenCarga-PDF');
   const option = {    
-    margin: [.5,0,0,0],
+    margin: [.5,3,0,0],
     filename: 'F-'+'.pdf',
-    image: {type: 'jpeg', quality: 1},
+    // image: {type: 'jpeg', quality: 1},
     html2canvas: {scale: 2, logging: true},
-    jsPDF: {unit: 'cm', format: 'letter', orientation: 'portrait'}, 
+    jsPDF: {unit: 'cm', format: 'letter', orientation: 'landscape'}, 
     pagebreak:{ avoid: '.pgbreak'}
   };
 
