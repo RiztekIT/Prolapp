e5bb4e0a33d7dfc47528b4474d31a36b7cd147f0
ivantalamantes <ivan.talamantes@live.com>
2020-06-03 17:36:54 -0600

    detalles facturacion

--- src/app/pages/cxc/complementopagocxc/recibo-pago/recibo-pago.component.ts
+++ src/app/pages/cxc/complementopagocxc/recibo-pago/recibo-pago.component.ts
@@ -942,7 +942,7 @@ console.log(this.json1);
 
     
     dia = `${days[fecha.getDate()]}`;
-    dia2 = `${days[fecha.getDate()-1]}`;
+    dia2 = `${days[fecha.getDate()+1]}`;
     mes = `${months[fecha.getMonth()]}`;
     año = fecha.getFullYear();
     hora = fecha.getHours();
@@ -984,7 +984,8 @@ console.log(this.json1);
 
   traerApi(fecha): Observable<any>{
 
-    return this.http.get("/SieAPIRest/service/v1/series/SF63528/datos/"+fecha+'/'+fecha, httpOptions)
+    //return this.http.get("/SieAPIRest/service/v1/series/SF63528/datos/"+fecha+'/'+fecha, httpOptions)
+    return this.http.get("/SieAPIRest/service/v1/series/SF60653/datos/"+fecha+'/'+fecha, httpOptions)
 
   }
 
