bf2da614d2c3653a288428437de18569fe7d2d4a
Rcdelarosa <ricardo.delarosa@riztek.com.mx>
2020-08-24 15:15:57 -0600

    .

--- src/app/pages/calidad/evidencias/evidencias-od/evidencias-od.component.ts
+++ src/app/pages/calidad/evidencias/evidencias-od/evidencias-od.component.ts
@@ -12,22 +12,22 @@ import { ImagenService } from 'src/app/services/imagenes/imagen.service';
 })
 export class EvidenciasODComponent implements OnInit {
 
- //variable para guardar el estatus de la Orden Carga
- estatusOD: string;
- FolioOD: string;
- OrigenOD: string;
-DestinoOD: string;
+  //variable para guardar el estatus de la Orden Carga
+  estatusOD: string;
+  FolioOD: string;
+  OrigenOD: string;
+  DestinoOD: string;
 
- IdOrdenDescarga: number;
+  IdOrdenDescarga: number;
 
- files: File[] = [];
- imagenes: any[];
- Folio: number;
+  files: File[] = [];
+  imagenes: any[];
+  Folio: number;
 
- imagePath: SafeResourceUrl;
- imageInfo: ImgInfo[] = [];
+  imagePath: SafeResourceUrl;
+  imageInfo: ImgInfo[] = [];
 
-  constructor(public router: Router, private _sanitizer: DomSanitizer ,public imageService: ImagenService, private OrdenDescargaService: OrdenDescargaService) { }
+  constructor(public router: Router, private _sanitizer: DomSanitizer, public imageService: ImagenService, private OrdenDescargaService: OrdenDescargaService) { }
 
   ngOnInit() {
     this.IdOrdenDescarga = +(localStorage.getItem('evidenciaOD'));
@@ -35,31 +35,31 @@ DestinoOD: string;
     this.ObtenerFolio(this.IdOrdenDescarga);
   }
 
-  getOrdenDescarga(){
-    this.OrdenDescargaService.getOrdenDescargaID(this.IdOrdenDescarga).subscribe( data=> {
+  getOrdenDescarga() {
+    this.OrdenDescargaService.getOrdenDescargaID(this.IdOrdenDescarga).subscribe(data => {
       console.log(data)
-          this.OrdenDescargaService.formData = data[0];
-          this.estatusOD = data[0].Estatus;
-          this.FolioOD = data[0].Folio;
-          this.OrigenOD = data[0].Origen;
-          this.DestinoOD = data[0].Destino;
-          console.log(this.estatusOD);
+      this.OrdenDescargaService.formData = data[0];
+      this.estatusOD = data[0].Estatus;
+      this.FolioOD = data[0].Folio;
+      this.OrigenOD = data[0].Origen;
+      this.DestinoOD = data[0].Destino;
+      console.log(this.estatusOD);
     });
   }
 
-  regresar(){
+  regresar() {
     this.router.navigate(['/evidencias']);
   }
 
-//Obtener Folio de Orden Descarga
-ObtenerFolio(id: number) {
-  this.OrdenDescargaService.getOrdenDescargaID(id).subscribe(dataOD => {
-    console.log(dataOD);
-    this.Folio = dataOD[0].Folio;
-    console.log(this.Folio);
-    this.leerDirImagenes();
-  })
-}
+  //Obtener Folio de Orden Descarga
+  ObtenerFolio(id: number) {
+    this.OrdenDescargaService.getOrdenDescargaID(id).subscribe(dataOD => {
+      console.log(dataOD);
+      this.Folio = dataOD[0].Folio;
+      console.log(this.Folio);
+      this.leerDirImagenes();
+    })
+  }
 
   leerDirImagenes() {
     //Obtener nombre de la imagen del servidor
@@ -69,105 +69,57 @@ ObtenerFolio(id: number) {
     this.imageInfo = new Array<ImgInfo>();
     this.files = [];
     console.log(this.imageInfo);
-    this.imageService.readDirImagenesServidor(formData,'cargarNombreImagenesOrdenDescarga').subscribe(res => {
-      if(res){
-      if (res.length > 0) {
-        console.log('Si hay imagenes')
-        console.log(res);
-        
-        for (let i = 0; i < res.length; i++) {
-          this.imagenes.push(res[i]);
-          let data = new ImgInfo;
-          data.ImageName = res[i];
-          console.log(data.ImageName);
-  
-          //Traer la imagen del servidor
-          const formDataImg = new FormData();
-          formDataImg.append('folio', this.Folio.toString())
-          formDataImg.append('archivo', data.ImageName)
-          console.log(formDataImg);
-          this.imageService.readImagenesServidor(formDataImg,'ObtenerImagenOrdenDescarga').subscribe(resImagen => {
-            console.log(resImagen);
-  
-            let TYPED_ARRAY = new Uint8Array(resImagen);
-            const STRING_CHAR = TYPED_ARRAY.reduce((data, byte) => {
-              return data + String.fromCharCode(byte);
-            }, '');
-            let base64String = btoa(STRING_CHAR);
-  
-            data.ImagePath = this._sanitizer.bypassSecurityTrustUrl('data:image/jpg;base64,' + base64String);
-            console.log(data.ImagePath);
-  
-            console.log(data);
-            this.imageInfo.push(data)
-          })
+    this.imageService.readDirImagenesServidor(formData, 'cargarNombreImagenesOrdenDescarga').subscribe(res => {
+      if (res) {
+        if (res.length > 0) {
+          console.log('Si hay imagenes')
+          console.log(res);
+
+          for (let i = 0; i < res.length; i++) {
+            this.imagenes.push(res[i]);
+            let data = new ImgInfo;
+            data.ImageName = res[i];
+            console.log(data.ImageName);
+
+            //Traer la imagen del servidor
+            const formDataImg = new FormData();
+            formDataImg.append('folio', this.Folio.toString())
+            formDataImg.append('archivo', data.ImageName)
+            console.log(formDataImg);
+            this.imageService.readImagenesServidor(formDataImg, 'ObtenerImagenOrdenDescarga').subscribe(resImagen => {
+              console.log(resImagen);
+
+              let TYPED_ARRAY = new Uint8Array(resImagen);
+              const STRING_CHAR = TYPED_ARRAY.reduce((data, byte) => {
+                return data + String.fromCharCode(byte);
+              }, '');
+              let base64String = btoa(STRING_CHAR);
+
+              data.ImagePath = this._sanitizer.bypassSecurityTrustUrl('data:image/jpg;base64,' + base64String);
+              console.log(data.ImagePath);
+
+              console.log(data);
+              this.imageInfo.push(data)
+            })
+          }
+          console.log(this.imageInfo)
+
+        } else {
+          console.log('No hay imagenes')
         }
-        console.log(this.imageInfo)
-  
-      } else {
-        console.log('No hay imagenes')
       }
-    }
     })
-  
+
   }
 
+  // descargar imagen
+  descargar(name) {
+    console.log(name.ImageName);
 
-  // download() {
-  //   const formData = new FormData();
-  //   formData.append('folio', this.Folio.toString())
-  //   this.imagenes = [];
-  //   this.imageInfo = new Array<ImgInfo>();
-  //   this.files = [];
-  //   console.log(this.imageInfo);
-  //   this.imageService.readDirImagenesServidor(formData,'cargarNombreImagenesOrdenDescarga').subscribe(res => {
-  //     if(res){
-  //     if (res.length > 0) {
-  //       console.log('Si hay imagenes')
-  //       console.log(res);
-        
-  //       for (let i = 0; i < res.length; i++) {
-  //         this.imagenes.push(res[i]);
-  //         let data = new ImgInfo;
-  //         data.ImageName = res[i];
-  //         console.log(data.ImageName);
-  
-  //         //Traer la imagen del servidor
-  //         const formDataImg = new FormData();
-  //         formDataImg.append('folio', this.Folio.toString())
-  //         formDataImg.append('archivo', data.ImageName)
-  //         console.log(formDataImg);
-  //         this.imageService.readImagenesServidor(formDataImg,'ObtenerImagenOrdenDescarga').subscribe(resImagen => {
-  //           console.log(resImagen);
-  
-  //           let TYPED_ARRAY = new Uint8Array(resImagen);
-  //           const STRING_CHAR = TYPED_ARRAY.reduce((data, byte) => {
-  //             return data + String.fromCharCode(byte);
-  //           }, '');
-  //           let base64String = btoa(STRING_CHAR);
-  
-  //           data.ImagePath = this._sanitizer.bypassSecurityTrustUrl('data:image/jpg;base64,' + base64String);
-  //           console.log(data.ImagePath);
-  
-  //           console.log(data);
-  //           this.imageInfo.push(data)
-  //         })
-  //       }
-  //       console.log(this.imageInfo)
-  
-  //     } else {
-  //       console.log('No hay imagenes')
-  //     }
-  //   }
-  //   })
-  // }
-  descargar(name){
- 
     const blobData = this.convertBase64ToBlobData(name.ImagePath.changingThisBreaksApplicationSecurity.toString().replace(/^data:image\/(png|jpeg|jpg);base64,/, ''));
-
     const blob = new Blob([blobData], { type: 'contentType' });
     const url = window.URL.createObjectURL(blob);
- 
+
     const link = document.createElement('a');
     link.href = url;
     link.download = name.ImageName;
@@ -177,26 +129,27 @@ ObtenerFolio(id: number) {
 
 
 
-  
-      convertBase64ToBlobData(base64Data: string, contentType: string='image/jpg', sliceSize=512) {
-        const byteCharacters = atob(base64Data);
-        const byteArrays = [];
-    
-        for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
-          const slice = byteCharacters.slice(offset, offset + sliceSize);
-    
-          const byteNumbers = new Array(slice.length);
-          for (let i = 0; i < slice.length; i++) {
-            byteNumbers[i] = slice.charCodeAt(i);
-          }
-    
-          const byteArray = new Uint8Array(byteNumbers);
-    
-          byteArrays.push(byteArray);
-        }
-    
-        const blob = new Blob(byteArrays, { type: contentType });
-        return blob;
+
+  convertBase64ToBlobData(base64Data: string, contentType: string = 'imagessssss/jpg', sliceSize = 512) {
+    const byteCharacters = atob(base64Data);
+    const byteArrays = [];
+
+    for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+      const slice = byteCharacters.slice(offset, offset + sliceSize);
+
+      const byteNumbers = new Array(slice.length);
+      for (let i = 0; i < slice.length; i++) {
+        byteNumbers[i] = slice.charCodeAt(i);
       }
+
+      const byteArray = new Uint8Array(byteNumbers);
+
+      byteArrays.push(byteArray);
+    }
+
+    const blob = new Blob(byteArrays, { type: contentType });
+    return blob;
+  }
+
 
 }
