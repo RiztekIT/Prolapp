9aea55de5867cf31274d34cb22365c526f36cb2c
Ivan Talamantes <ivan.talamantes@riztek.com.mx>
2020-10-18 20:27:25 -0600

    merge

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -32,8 +32,8 @@ const httpOptions = {
 })
 export class HeaderComponent implements OnInit {
   // parche: string = 'https://cors-anywhere.herokuapp.com/'
-   readonly rootURL = "https://www.banxico.org.mx/SieAPIRest/service/v1/series/SF63528/datos/oportuno"
-  // rootURL = "/SieAPIRest/service/v1/series/SF63528/datos/"
+  // readonly rootURL = "https://www.banxico.org.mx/SieAPIRest/service/v1/series/SF63528/datos/oportuno"
+   rootURL = "/SieAPIRest/service/v1/series/SF63528/datos/"
   //rootURL = "/SieAPIRest/service/v1/series/SF63528/datos/"
   Cdolar: string;
   clienteLogin;
