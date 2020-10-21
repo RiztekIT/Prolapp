c3949cc72f428e2bcb2b9258666f2eb43a37b835
sierra_javier <javier.sierra@riztek.com.mx>
2020-10-21 15:57:56 -0600

    ...

--- src/app/services/almacen/tarima/tarima.service.ts
+++ src/app/services/almacen/tarima/tarima.service.ts
@@ -10,8 +10,8 @@ import { DetalleOrdenDescarga } from '../../../Models/almacen/OrdenDescarga/deta
 import { Usuario } from 'src/app/Models/catalogos/usuarios-model';
 import { MasterDetalleTarima } from 'src/app/Models/almacen/OrdenDescarga/cuu/masterDetalleTarima-model';
 
-// export const APIUrl = environment.APIUrl;
-export const APIUrl = 'https://localhost:44361/api';
+export const APIUrl = environment.APIUrl;
+// export const APIUrl = 'https://localhost:44361/api';
 
 @Injectable({
   providedIn: 'root'
