861e3d1ca7ce1559d5257b66d4fcad57486813a9
Sierra-Javier <javier.sierra@riztek.com.mx>
2021-03-25 17:39:21 -0700

    traspaso

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -19,11 +19,7 @@ export const environment = {
   APIUrlEmail: 'https://riztekserver.ddns.net:3000' 
   // URLS de Produccion
     //  APIUrl: 'https://erpprolapp.ddns.net:44361/api',
-<<<<<<< Updated upstream
    //    APIUrlEmail: 'https://erpprolapp.ddns.net:3000'
-=======
-      //  APIUrlEmail: 'https://erpprolapp.ddns.net:3000'
->>>>>>> Stashed changes
     // APIUrl: 'https://erpprolapp.ddns.net:44361/api',
     //APIUrlEmail: 'https://erpprolapp.ddns.net:3000'
 };
